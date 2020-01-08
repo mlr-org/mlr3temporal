47a9be65821592dd0d04bfe5680fb1c924299683
corneliagru <cornelia.gru@gmail.com>
2020-01-08 15:26:26 +0100

    add airpassenger example

--- example.r
+++ example.r
@@ -1,9 +1,16 @@
 library(digest)
 library(tsbox)
-####### Example univariate forecast
+### Example univariate forecast
 
 df = ts_c(mdeaths,fdeaths)
 task = TaskRegrForecast$new(id = "a",backend =df,target = "mdeaths")
+learner=LearnerRegrForecastAutoArima$new()
+learner$train(task)
+learner$model
+
+### Example 2
+
+task = tsk("AirPassengers")
 learner=LearnerRegrForecastAutoArima$new()
 learner$train(task)
 learner$model
