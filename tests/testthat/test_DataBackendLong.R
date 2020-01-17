d665b85510b1aeb823fab222541ef8e469c97043
corneliagru <cornelia.gru@gmail.com>
2020-01-17 11:07:03 +0100

    remove my version

--- tests/testthat/test_DataBackendLong.R
+++
@@ -1,9 +0,0 @@
-# context("DataBackendLong")
-## this is not recommended anymore see: https://rdrr.io/cran/testthat/man/context.html
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
-
-
