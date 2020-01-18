bacd3ac6671307fb57c9d6a5b5259d462b0776a0
Johannes Nawrath <johannes-nawrath@t-online.de>
2020-01-18 17:58:47 +0100

    Fix in the tasks to allow multivariate targets.

--- R/TaskForecasting.R
+++ R/TaskForecasting.R
@@ -2,16 +2,22 @@ TaskForecast = R6::R6Class("TaskForecast",
   inherit = TaskSupervised,
   public = list(
     initialize = function(id, backend, target, time) {
-      assert_string(target)
+      assert_character(target)
+      assert_string(time)
       super$initialize(id = id, task_type = "forecast", backend = backend, target = target)
 
-      type = self$col_info[id == target]$type
-      if (type %nin% c("integer", "numeric")) {
-        stopf("Target column '%s' must be numeric", target)
+      for (i in self$target_names){
+        type = self$col_info[id == i]$type
+        if (type %nin% c("integer", "numeric")) {
+          stopf("Target column '%s' must be numeric", i)
+        }
       }
 
       assert_choice(time, self$backend$colnames)
-      self$col_roles$order = time
+
+      # FIXME: time is the primary key; the primary key can't have a role
+      # self$col_roles$order = time
+
     },
 
     truth = function(row_ids = NULL) {
