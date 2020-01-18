a521cf5a5d386063f7b4adb873b8a720246121bc
Florian <pfistfl@users.noreply.github.com>
2020-01-18 13:37:11 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -4,5 +4,5 @@ if (ci_has_env("BUILD_PKGDOWN")) {
   do_pkgdown(orphan = TRUE)
 }
 
-get_stage("after_success") %>%
-  add_code_step(system("bash ./inst/trigger-mlr3book.sh"))
+# get_stage("after_success") %>%
+#  add_code_step(system("bash ./inst/trigger-mlr3book.sh"))
