4f3582b6466e6124317083f22aecd6536c2842e5
Arshadipour <57601721+Arshadipour@users.noreply.github.com>
2020-03-02 15:26:29 +0100

    Description for LearnerForecastAverage

--- R/LearnerForecastAverage.R
+++ R/LearnerForecastAverage.R
@@ -1,3 +1,19 @@
+#' @title Average Learner
+#'
+#' @name mlr_learners_regr.Average
+#' @format [R6::R6Class] inheriting from [mlr3::LearnerRegr].
+#'
+#' @section Construction:
+#' ```
+#' LearnerForecastAverage$new()
+#' ```
+#'
+#' @description
+#' A LearnerRegrForecast for model based on average implemented in [mlr3::mlr_learners_forecast.average] in mlr3.
+#'
+#'
+#' @template seealso_learner
+#' @export
 LearnerForecastAverage = R6::R6Class("LearnerForecastAverage", inherit = LearnerForecast,
   public = list(
     initialize = function() {
