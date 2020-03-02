5c35e29822326c647373e198ad4c15d68a912121
Arshadipour <57601721+Arshadipour@users.noreply.github.com>
2020-03-02 15:25:17 +0100

    Description for LearnerForecast

--- R/LearnerForecast.R
+++ R/LearnerForecast.R
@@ -1,3 +1,31 @@
+#' @title LearnerForecast
+#'
+#' @usage NULL
+#' @name LearnerForecast
+#' @format [R6::R6Class] inheriting from [mlr3::LearnerRegr].
+#'
+#' @section Construction:
+#' ```
+#' LearnerForecast$new()
+#' ```
+#'
+#' @description
+#' A LearnerForecast for for learner objects like [LearnerRegr].
+#'
+#'
+#' Learners are build around the three following key parts:
+#'
+#' * Methods `$train()` and `$predict()` which call internal methods.
+#' * A [paradox::ParamSet] which stores meta-information about available hyperparameters, and also stores hyperparameter settings.
+#' * Meta-information about the requirements and capabilities of the learner.
+#' * The fitted model stored in field `$model`, available after calling `$train()`.
+#'
+#' More classification and regression learners are implemented in the add-on package \CRANpkg{mlr3learners}.
+#' More (experimental) learners can be found on GitHub: \url{https://github.com/mlr3forecasting/}.
+#' A guide on how to extend \CRANpkg{mlr3} with custom learners can be found in the [mlr3book](https://mlr3book.mlr-org.com).
+#'
+#' @template seealso_learner
+#' @export
 LearnerForecast = R6Class("LearnerForecast", inherit = Learner,
   public = list(
     date_span = NULL,
