dcc2f593c21aee628abf68491608664149fbbbb5
Maximilian Muecke <muecke.maximilian@gmail.com>
2023-06-03 23:19:18 +0200

    feat: migrated databackendlong to R6 docs

--- R/DataBackendLong.R
+++ R/DataBackendLong.R
@@ -1,111 +1,54 @@
-#' @title DataBackend
-#'
-#' @usage NULL
-#' @format  [R6::R6Class] inheriting from [mlr3::DataBackend]
+#' @title DataBackend for timeseries in long-format
 #'
 #' @description
-#' DataBackend for timeseries in long-format
+#' [DataBackend] for \CRANpkg{data.table} which serves as an efficient in-memory data base.
 #'
-#' Data Backends provide a layer of abstraction for various data storage systems.
-#' It is not recommended to work directly with the DataBackend.
-#' Instead, all data access is handled transparently via the [Task].
+#' @template param_rows
+#' @template param_cols
+#' @template param_data_format
+#' @template param_primary_key
+#' @template param_na_rm
 #'
-#' To connect to out-of-memory database management systems such as SQL servers, see \CRANpkg{mlr3db}.
-#'
-#' The required set of fields and methods to implement a custom DataBackend is listed in the respective sections.
-#' See [DataBackendDataTable] or [DataBackendMatrix] for exemplary implementations of the interface.
-#'
-#' @section Construction:
-#' Note: This object is typically constructed via a derived classes, e.g. [DataBackendDataTable] or [DataBackendMatrix],
-#' or via the S3 method [as_data_backend()].
-#'
-#' ```
-#' DataBackend$new(data, primary_key = NULL, data_formats = "data.table")
-#' ```
-#'
-#' * `data` :: `any`\cr
-#'   The format of the input data depends on the specialization.
-#'   E.g., [DataBackendDataTable] expects a [data.table::data.table()] and [DataBackendMatrix] expects a [Matrix::Matrix()]
-#'   constructed with the \CRANpkg{Matrix} package.
-#'
-#' * `primary_key` :: `character(1)`\cr
-#'   Each DataBackend needs a way to address rows, which is done via a column of unique values, referenced here by `primary_key`.
-#'   The use of this variable may differ between backends.
-#'
-#' * data_formats (`character()`)\cr
-#'   Set of supported formats, e.g. `"data.table"` or `"Matrix"`.
-#'
-#' @section Fields:
-#' * `nrow` :: `integer(1)`\cr
-#'   Number of rows (observations).
-#'
-#' * `ncol` :: `integer(1)`\cr
-#'   Number of columns (variables), including the primary key column.
-#'
-#' * `colnames` :: `character()`\cr
-#'   Returns vector of all column names, including the primary key column.
-#'
-#' * `rownames` :: (`integer()` | `character()`)\cr
-#'   Returns vector of all distinct row identifiers, i.e. the primary key column.
-#'
-#' * `hash` :: `character(1)`\cr
-#'   Returns a unique hash for this backend. This hash is cached.
-#'
-#' * `data_formats` :: `character()`\cr
-#'   Vector of supported data formats.
-#'   A specific format can be chosen in the `$data()` method.
-#'
-#' @section Methods:
-#' * `data(rows = NULL, cols = NULL, format = "data.table")`\cr
-#'   (`integer()` | `character()`, `character()`) -> `any`\cr
-#'   Returns a slice of the data in the specified format.
-#'   Currently, the only supported formats are `"data.table"` and `"Matrix"`.
-#'   The rows must be addressed as vector of primary key values, columns must be referred to via column names.
-#'   Queries for rows with no matching row id and queries for columns with no matching column name are silently ignored.
-#'   Rows are guaranteed to be returned in the same order as `rows`, columns may be returned in an arbitrary order.
-#'   Duplicated row ids result in duplicated rows, duplicated column names lead to an exception.
-#'
-#' * `distinct(rows, cols, na_rm = TRUE)`\cr
-#'   (`integer()` | `character()`, `character()`, `logical(1)`) -> named `list()`\cr
-#'   Returns a named list of vectors of distinct values for each column specified.
-#'   If `na_rm` is `TRUE`, missing values are removed from the returned vectors of distinct values.
-#'   Non-existing rows and columns are silently ignored.
-#'
-#'   If `rows` is `NULL`, all possible distinct values will be returned, even if the value is not present in the data.
-#'   This affects factor-like variables with empty levels, if supported by the backend.
-#'
-#' * `head(n = 6)`\cr
-#'   `integer(1)` -> [data.table::data.table()]\cr
-#'   Returns the first up-to `n` rows of the data as [data.table::data.table()].
-#'
-#' * `missings(rows, cols)`\cr
-#'   (`integer()` | `character()`, `character()`) -> named `integer()`\cr
-#'   Returns the number of missing values per column in the specified slice of data.
-#'   Non-existing rows and columns are silently ignored.
-#'
-#' @family DataBackend
-#' @seealso
-#' Extension Packages: \CRANpkg{mlr3db}
+#' @template seealso_databackend
 #' @export
-# @examples
-# data = data.table::data.table(id = 1:5, x = runif(5), y = sample(letters[1:3], 5, replace = TRUE))
-#
-# b = DataBackendDataTable$new(data, primary_key = "id")
-# print(b)
-# b$head(2)
-# b$data(rows = 1:2, cols = "x")
-# b$distinct(rows = b$rownames, "y")
-# b$missings(rows = b$rownames, cols = names(data))
+#' @examples
+#' data = data.table::data.table(id = 1:5, x = runif(5), y = sample(letters[1:3], 5, replace = TRUE))
+#' b = mlr3::DataBackendDataTable$new(data, primary_key = "id")
+#' print(b)
+#' b$head(2)
+#' b$data(rows = 1:2, cols = "x")
+#' b$distinct(rows = b$rownames, "y")
+#' b$missings(rows = b$rownames, cols = names(data))
 DataBackendLong = R6::R6Class("DataBackendLong",
-
-  inherit = mlr3::DataBackend,
+  inherit = DataBackend,
   cloneable = FALSE,
-
   public = list(
     id_col = NULL,
+    #' @field value_col (`character()`)\cr
+    #' Names of the columns containing the values.
+
+    #' @field date_col (`character(1)`)\cr
+    #' Name of the column containing the timestamps.
     value_col = NULL,
+
+    #' @field id_col (`character(1)`)\cr
+    #' Name of the column containing the row ids.
     date_col = NULL,
 
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    #'
+    #' Note that `DataBackendLong` does not copy the input data, while `as_data_backend()` calls [data.table::copy()].
+    #' `as_data_backend()` also takes care about casting to a `data.table()` and adds a primary key column if necessary.
+    #'
+    #' @param data ([data.table::data.table()])\cr
+    #'   The input [data.table()].
+    #'
+    #' @param id_col (`character(1)`)\cr
+    #'   Name of the column containing the row ids.
+    #'
+    #' @param date_col (`character(1)`)\cr
+    #'   Name of the column containing the timestamps.
     initialize = function(data, primary_key, id_col, date_col) {
       assert_data_frame(data, ncols = 4L, col.names = "unique")
       setDT(data)
@@ -117,6 +60,16 @@ DataBackendLong = R6::R6Class("DataBackendLong",
       setkeyv(data, c(primary_key, id_col))
     },
 
+    #' @description
+    #' Returns a slice of the data in the specified format.
+    #' Currently, the only supported formats are `"data.table"` and `"Matrix"`.
+    #' The rows must be addressed as vector of primary key values, columns must be referred to via column names.
+    #' Queries for rows with no matching row id and queries for columns with no matching column name are silently ignored.
+    #' Rows are guaranteed to be returned in the same order as `rows`, columns may be returned in an arbitrary order.
+    #' Duplicated row ids result in duplicated rows, duplicated column names lead to an exception.
+    #'
+    #' @param roll (`logical(1)`)\cr
+    #'  If `TRUE` (default), rows are rolled to the nearest existing row id if necessary.
     data = function(rows, cols, data_format = "data.table", roll = TRUE) {
       assert_atomic_vector(rows)
       assert_names(cols, type = "unique")
@@ -125,29 +78,49 @@ DataBackendLong = R6::R6Class("DataBackendLong",
       cols = intersect(cols, self$colnames)
       rows = keep_in_bounds(rows, 1L, max(private$.data[[self$primary_key]]))
 
-      if (length(cols) == 0)
+      if (length(cols) == 0) {
         return(data.table()) # FIXME: Not sure what should be returned here. cols = "_not_existing_" check.
-      if (!all(cols %in% self$key_cols))
+      }
+      if (!all(cols %in% self$key_cols)) {
         subset_cols = setdiff(cols, self$key_cols) # for dcasting id_col
-      else
+      } else {
         subset_cols = cols # case we only want primary key or date
+      }
       # FIXME: This is not very efficient, but seems reasonably robust
 
-      if (length(rows) != 0L)
+      if (length(rows) != 0L) {
         dt = private$.data[CJ(rows, subset_cols), roll = roll]
-      else # else: keep all rows, subset later
+      } else {
+        # keep all rows, subset later
         dt = private$.data[CJ(private$.data[[self$primary_key]], subset_cols), roll = roll]
-      dt = dcast(dt, formulate(paste0(c(self$key_cols), collapse = "+"), self$id_col), fun.aggregate = identity, fill = NA)
+      }
+      dt = dcast(
+        dt, formulate(paste0(c(self$key_cols), collapse = "+"), self$id_col), fun.aggregate = identity, fill = NA
+      )
       dt = dt[list(rows), cols, with = FALSE, on = self$primary_key, nomatch = 0L]
       return(dt)
     },
 
+    #' @description
+    #' Retrieve the first `n` rows.
+    #'
+    #' @param n (`integer(1)`)\cr
+    #'   Number of rows.
+    #'
+    #' @return [data.table::data.table()] of the first `n` rows.
     head = function(n = 6L) {
       rn = head(self$rownames, n)
       cn = self$colnames
       self$data(rn, cn)
     },
 
+    #' @description
+    #' Returns a named list of vectors of distinct values for each column
+    #' specified. If `na_rm` is `TRUE`, missing values are removed from the
+    #' returned vectors of distinct values. Non-existing rows and columns are
+    #' silently ignored.
+    #'
+    #' @return Named `list()` of distinct values.
     distinct = function(rows, cols, na_rm = TRUE) {
       assert_names(cols, type = "unique")
       assert_flag(na_rm)
@@ -161,6 +134,11 @@ DataBackendLong = R6::R6Class("DataBackendLong",
       }
     },
 
+    #' @description
+    #' Returns the number of missing values per column in the specified slice
+    #' of data. Non-existing rows and columns are silently ignored.
+    #'
+    #' @return Total of missing values per column (named `numeric()`).
     missings = function(rows, cols) {
       assert_names(cols, type = "unique")
       data = self$data(rows, cols)
@@ -169,21 +147,32 @@ DataBackendLong = R6::R6Class("DataBackendLong",
   ),
 
   active = list(
+    #' @field rownames (`integer()`)\cr
+    #' Returns vector of all distinct row identifiers, i.e. the contents of the primary key column.
     rownames = function() {
       unique(private$.data[, self$primary_key, with = FALSE])[[1L]]
     },
 
+    #' @field colnames (`character()`)\cr
+    #' Returns vector of all column names, including the primary key column.
     colnames = function() {
       c(self$primary_key, self$date_col, unique(private$.data[, self$id_col, with = FALSE])[[1L]])
     },
 
+    #' @field nrow (`integer(1)`)\cr
+    #' Number of rows (observations).
     nrow = function() {
       uniqueN(private$.data, by = self$primary_key)
     },
 
+    #' @field ncol (`integer(1)`)\cr
+    #' Number of columns (variables), including the primary key column.
     ncol = function() {
       uniqueN(private$.data, by = self$id_col) + 2L
     },
+
+    #' @field key_cols (`character()`)\cr
+    #' Returns vector of all column names that are part of the primary key.
     key_cols = function() {
       c(self$primary_key, self$date_col)
     }
@@ -217,7 +206,7 @@ as_data_backend.dts = function(data, primary_key = NULL, target = NULL, ...) {
 
 #' @rdname as_data_backend
 #' @export
-as_data_backend.forecast = function(data, primary_key = NULL, ...) {
+as_data_backend.forecast = function(data, primary_key = NULL, ...) { # nolint
   require_namespaces("tsbox")
   as_data_backend(tsbox::ts_dts(data))
 }
